492ffb7bb7fe2935b0a904c110615ca9e7e949be
Rayan Nadeem <rayan.nadeem@outlook.com>
2022-04-22 13:07:44 +0100

    removed bloat

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,43 @@ tue
 */
 #![allow(unused_variables)]
 use std::str::ParseBoolError;
-// use json::object; 
-use serde::Serialize;
+// use json::object;
 use serde::Deserialize;
+use serde::Serialize;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
+use std::env;
 
+// Data structure for each day, storing how the day went and the date.
 #[derive(Serialize, Deserialize, Debug)]
 struct Day {
     date: String,
-    feeling_good: bool
+    feeling_good: bool,
 }
 
+// Settings for the user, changeable at any time (hopefully)
 struct Settings {
     admin: bool,
-    times_to_enter: u8 
+    times_to_enter: u8,
 }
 
 impl Settings {
-    fn new(admin_buffer: bool, times_to_enter_buffer: u8)  -> Settings{
+    // Default settings for the user to use
+    fn new(admin_buffer: bool, times_to_enter_buffer: u8) -> Settings {
         Settings {
             admin: admin_buffer,
-            times_to_enter: times_to_enter_buffer
+            times_to_enter: times_to_enter_buffer,
         }
     }
 }
 
-fn init_settings (admin_setting: bool, times: u8) -> Settings{
-    Settings::new(admin_setting, times)
-}
-
+// 
 fn input_system(list: &mut Vec<Day>, settings: &Settings) {
-        
     let mut count = 0;
 
-    'main: loop{
-        
+    'main: loop {
         if count == settings.times_to_enter {
             break;
         }
@@ -53,19 +52,19 @@ fn input_system(list: &mut Vec<Day>, settings: &Settings) {
         std::io::stdin().read_line(&mut input).unwrap();
 
         let input_slice: &str = &input[..];
-        let t:Result<bool, ParseBoolError>= input_slice.trim().parse();
+        let t: Result<bool, ParseBoolError> = input_slice.trim().parse();
 
         match t {
             Ok(input) => {
                 println!("processing...");
 
-                let mut buffer = String::new(); 
+                let mut buffer = String::new();
 
                 println!("date: ");
-                
+
                 std::io::stdin().read_line(&mut buffer).unwrap();
 
-                buffer = buffer.trim().to_string(); 
+                buffer = buffer.trim().to_string();
 
                 for day in list.iter() {
                     if day.date == buffer {
@@ -74,110 +73,120 @@ fn input_system(list: &mut Vec<Day>, settings: &Settings) {
                     }
                 }
 
-                list.push(Day{date: buffer, feeling_good: input});
+                list.push(Day {
+                    date: buffer,
+                    feeling_good: input,
+                });
+            }
 
-            },
-
-            Err(err) => {println!("Something went wrong");}
+            Err(err) => {
+                println!("Something went wrong");
+            }
         }
     }
-
 }
 
-fn write_to_file(list: &Vec<Day>){
+fn write_to_file(list: &Vec<Day>) {
     let mut file = File::create("save.json").expect("failed to create file");
 
-    let serialised = serde_json::to_string(list).unwrap();
+    let serialised = serde_json::to_string_pretty(list).unwrap();
 
-    file.write_all(&serialised.into_bytes()).expect("failed to write to file"); 
+    
 
+    file.write_all(&serialised.into_bytes())
+        .expect("failed to write to file");
 }
 
 fn main() {
-    // File reader and creat-er system thingy COPYRIGHT TheAverageAvocado 2022 https://github.com/LittlRayRay 
-   
-    let settings = init_settings(true, 2);
 
-    let mut exists: bool= false;
+    let args: String = env::args().collect();
+    println!("{:?}", args);
+
+    
+
+    // File reader and creat-er system thingy COPYRIGHT TheAverageAvocado 2022 https://github.com/LittlRayRay
+
+    let settings = Settings::new(true, 2);
+
+    let mut exists: bool = false;
     let mut list: Vec<Day> = Vec::new();
 
-    if Path::new("save.json").exists(){
+    if Path::new("save.json").exists() {
         exists = true;
     }
-    
+
     if exists {
-        let reader = BufReader::new(File::open("save.json").expect("failed to read save.json")); 
-        list= serde_json::from_reader(reader).expect("file exists, could not read it");
+        let reader = BufReader::new(File::open("save.json").expect("failed to read save.json"));
+        list = serde_json::from_reader(reader).expect("file exists, could not read it");
     }
 
     input_system(&mut list, &settings);
 
     // Now we can insert our logic for the actual system here
-    // TODO Transfer old system, mostly same code. 
+    // TODO Transfer old system, mostly same code.
 
     write_to_file(&list);
 
     /*
 
-    let file = File::create("test.json").expect("failed to open file");
-    
-    //let file_read = File::open("save.json").expect("failed to open file");
-    let reader = BufReader::new(file);
+     let file = File::create("test.json").expect("failed to open file");
 
-    let mut list: Vec<Day>= serde_json::from_reader(reader).expect("read file");
-    
-    //let mut list: Vec<Day> = Vec::new();
-    let mut i = 0;
-    while i < 2{
-        'main: loop {
-            println!("Are you feeling good today?");
-            let mut input = String::new();
-              
-            std::io::stdin().read_line(&mut input).unwrap();
-              
-            let input_slice: &str = &input[..];
-            let t:Result<bool, ParseBoolError>= input_slice.trim().parse();
+     //let file_read = File::open("save.json").expect("failed to open file");
+     let reader = BufReader::new(file);
 
-            match t {
-                Ok(input) => {
-                    println!("Thanks for the input!");
+     let mut list: Vec<Day>= serde_json::from_reader(reader).expect("read file");
 
-                    let mut buffer = String::new();
+     //let mut list: Vec<Day> = Vec::new();
+     let mut i = 0;
+     while i < 2{
+         'main: loop {
+             println!("Are you feeling good today?");
+             let mut input = String::new();
 
-                    println!("Please enter today's date (YYYY-MM-DD)!");
+             std::io::stdin().read_line(&mut input).unwrap();
 
-                    std::io::stdin().read_line(&mut buffer).unwrap();
+             let input_slice: &str = &input[..];
+             let t:Result<bool, ParseBoolError>= input_slice.trim().parse();
 
-                    buffer = buffer.trim().to_string();
-                    
-                    for day in &list{
-                        if day.date == buffer {
-                            println!("You have already done today's moodtracker!");
-                            break 'main;
-                        }
+             match t {
+                 Ok(input) => {
+                     println!("Thanks for the input!");
 
-                    }
+                     let mut buffer = String::new();
 
-                    list.push(Day{date:buffer, feeling_good: input });
+                     println!("Please enter today's date (YYYY-MM-DD)!");
 
-                    break;
-                    
-                },
+                     std::io::stdin().read_line(&mut buffer).unwrap();
 
-                Err(_error) => {println!("Something went wrong!"); println!("Please enter 'true' or 'false'!")}
-            }
-        }
-        i += 1;
-     }
+                     buffer = buffer.trim().to_string();
 
-    println!("{:?}", list);
+                     for day in &list{
+                         if day.date == buffer {
+                             println!("You have already done today's moodtracker!");
+                             break 'main;
+                         }
 
-    let serialised= serde_json::to_string(&list).unwrap();
+                     }
 
-    println!("serialised = {}", serialised);
+                     list.push(Day{date:buffer, feeling_good: input });
 
-   // file.write_all(&serialised.into_bytes()).expect("failed to write to file");
-    
-    */
+                     break;
 
+                 },
+
+                 Err(_error) => {println!("Something went wrong!"); println!("Please enter 'true' or 'false'!")}
+             }
+         }
+         i += 1;
+      }
+
+     println!("{:?}", list);
+
+     let serialised= serde_json::to_string(&list).unwrap();
+
+     println!("serialised = {}", serialised);
+
+    // file.write_all(&serialised.into_bytes()).expect("failed to write to file");
+
+     */
 }
